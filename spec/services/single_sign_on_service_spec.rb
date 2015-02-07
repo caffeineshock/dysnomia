61726a79008e7a7ed2231966669b841c1f183f31
Arne <caffeine@parttimegeeks.net>
2015-02-07 14:26:16 +0100

    change indent

--- spec/services/single_sign_on_service_spec.rb
+++ spec/services/single_sign_on_service_spec.rb
@@ -4,15 +4,13 @@ describe SingleSignOnService do
   describe '.redirect_url' do
   	subject { SingleSignOnService.new }
 
-  	it 'should return url if request was generated with correct key'
-
     it 'should throw exception if request was generated without sso parameter' do
-      expect{ subject.redirect_url }.to raise_error
+      expect { subject.redirect_url }.to raise_error
     end
 
     it 'should throw exception if request was generated with incorrect key' do
       subject.query_string = "?sso=&sig="
-      expect{ subject.redirect_url }.to raise_error
+      expect { subject.redirect_url }.to raise_error
     end
   end
 end
