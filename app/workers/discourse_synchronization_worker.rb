53c4f841834100be6980c4c18b0d605019f45155
Arne <caffeine@parttimegeeks.net>
2015-02-11 10:54:31 +0100

    use sync_sso instead of individual methods in sync worker

--- app/workers/discourse_synchronization_worker.rb
+++ app/workers/discourse_synchronization_worker.rb
@@ -1,16 +1,21 @@
 class DiscourseSynchronizationWorker < DiscourseWorker
   include Sidekiq::Worker
 
-  def perform(user_id, username)
+  def perform(user_id)
   	for_each_tenant(user_id) do |u,t|
       client(t) do |c|
-      	c.update_email(username, u.email)
+        c.sso_sync(
+          sso_secret: t.settings(:discourse).sso_secret
+          name: u.username
+          username: u.username
+          email: u.email
+          external_id: user_id
+        )
         c.update_avatar(username: username, file: t.url + u.avatar.url)
-        c.update_username(username, u.username)
       end
     end
   rescue DiscourseApi::Error
-    logger.warn "User '#{username}' couldn't be saved by discourse (does a discourse account exist?)"
+    logger.warn "User with ID '#{user_id}' couldn't be saved by discourse (does a discourse account exist?)"
   end
 
   private 
