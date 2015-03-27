7be0648581e9e5015e91a22b9dc616590ea24c7a
Arne <caffeine@parttimegeeks.net>
2015-03-27 17:17:26 +0100

    Add method that was accidently removed

--- app/decorators/tenant_decorator.rb
+++ app/decorators/tenant_decorator.rb
@@ -1,6 +1,10 @@
 class TenantDecorator < Draper::Decorator
   delegate_all
 
+  def link
+    h.link_to object.name, object
+  end
+
   def title
     object.logo.exists? ? h.image_tag(object.logo.url(:navbar)) : object.name
   end
