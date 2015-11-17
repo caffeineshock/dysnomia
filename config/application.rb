d895c887d09ceeb60d98db4f8d806c315bad44d1
Arne <caffeine@parttimegeeks.net>
2015-11-17 13:01:35 +0100

    Update gems

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(:default, Rails.env)
 
 module Dysnomia
   class Application < Rails::Application
+    # Make Settings available before rails application initialization process
+    Config::Integration::Rails::Railtie.preload
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
