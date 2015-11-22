97ea0b7781a3ed26a122b82087f86afa9dc41b09
Arne <caffeine@parttimegeeks.net>
2015-11-23 00:22:11 +0100

    handle channels in search correctly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ApplicationController
 
   def search query
   	PgSearch.multisearch(query).order(:updated_at).to_a.delete_if do |r|
-  	  r.searchable.nil? or r.searchable.is_a? Channel and r.searchable.visible_for? current_user
+  	  r.searchable.nil? or (r.searchable.is_a? Channel and r.searchable.visible_for? current_user)
   	end
   end
 end
